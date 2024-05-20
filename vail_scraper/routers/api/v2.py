89cdc4119ae6ddd1868c42dc3d913768ed7f07c3
TAG-Epic <tagepicuwu@gmail.com>
2024-05-20 17:26:44 +0200

    feat: user count endpoint

--- vail_scraper/routers/api/v2.py
+++ vail_scraper/routers/api/v2.py
@@ -532,3 +532,14 @@ async def get_stats_for_user_v2(request: web.Request) -> web.StreamResponse:
             },
         }
     )
+
+@router.get("/api/v2/game/user-count")
+@api_cors
+async def get_user_count(request: web.Request) -> web.StreamResponse:
+    db = request.app[app_keys.DATABASE]
+
+    result = await db.execute("select count(*) from users")
+    row = await result.fetchone()
+    assert row is not None, "missing result from count(*) query"
+    
+    return web.json_response({"count": row[0]})
