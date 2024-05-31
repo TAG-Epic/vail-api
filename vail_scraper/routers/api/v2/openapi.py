9c5ba73645b52d803891008ec3b9c79d1bdc7891
TAG-Epic <tagepicuwu@gmail.com>
2024-06-01 01:29:09 +0200

    fix: cors for openapi

--- vail_scraper/routers/api/v2/openapi.py
+++ vail_scraper/routers/api/v2/openapi.py
@@ -3,9 +3,12 @@ from pathlib import Path
 from aiohttp import web
 import yaml
 
+from ....utils.cors import api_cors
+
 router = web.RouteTableDef()
 
 @router.get("/api/v2/openapi.yaml")
+@api_cors
 async def get_openapi(request: web.Request) -> web.StreamResponse:
     del request # Unused
     current_directory = Path(__file__).parent
